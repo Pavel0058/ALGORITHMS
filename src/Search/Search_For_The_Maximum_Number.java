eba73a78280369739321f132fd3a08854c4b2972
Pavel0058 <pavel_koldashev@mail.ru>
2023-06-15 13:35:51 +0300

    edit text

--- src/Search/Search_For_The_Maximum_Number.java
+++ src/Search/Search_For_The_Maximum_Number.java
@@ -4,7 +4,7 @@ public class Search_For_The_Maximum_Number {
     public static void main(String[] args) {
         int[] arr = {7, 3, 9, 2, 5, 15};
         int max = findMaxNumber(arr);
-        System.out.println("Максимальное число " + max);
+        System.out.println("Максимальное число  = " + max);
     }
     public static int findMaxNumber(int[] arr) {
         int max = arr[0];
